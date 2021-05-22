83026a0c03113e9574ce749deeb98517f2d8a826
rd-code-23 <70293845+rd-code-23@users.noreply.github.com>
2021-05-21 19:45:36 -0700

    refactor

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,5 +3,8 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true,
 })
+.then(() => console.log('db working'))
+.catch((error) => console.log(error))
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const cors = require('cors')
-//require('./db/mongoose')
+require('./db/mongoose')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 const port = process.env.PORT
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
