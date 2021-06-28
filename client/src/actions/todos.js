4a942e5a0253ee8a80ba586114c83a7a539dc146
rd-code-23 <70293845+rd-code-23@users.noreply.github.com>
2021-06-28 10:24:39 -0700

    refactor todos  action

--- client/src/actions/todos.js
+++ client/src/actions/todos.js
@@ -2,34 +2,21 @@ import * as api from '../api/api';
 
 export const getTodos = async (dispatch) => {
     try {
-        console.log();
         const { data } = await api.getTodos();
-
-        // console.log('Recieved data: ', data); //should get same results as postman login 
-
         dispatch({ type: 'SET_TODOS', payload: data });
         return true;
-        // router.push('/');
     } catch (error) {
-        console.log(error);
         return false;
     }
 };
 
 export const addTodo = async (todo, dispatch) => {
     try {
-        console.log('adding todo: ', todo);
-        const todo2 = { text: todo, completed: false }
-        const { data } = await api.addTodo(todo2);
-
-
-        //   console.log('Recieved data: ', data); //should get same results as postman 
-
-        dispatch({ type: 'ADD_TODO', payload: todo })
+        const todoData = { text: todo, completed: false }
+        const { data } = await api.addTodo(todoData);
+        dispatch({ type: 'ADD_TODO', payload: todo });
         return true;
-        // router.push('/');
     } catch (error) {
-        console.log(error);
         return false;
     }
 };
@@ -37,23 +24,10 @@ export const addTodo = async (todo, dispatch) => {
 //for todos user added before sign in 
 export const saveCurrentTodos = async (todos, dispatch) => {
     try {
-        console.log(todos);
         for (const todo of todos) {
             await addTodo(todo.text, dispatch);
         }
-        // console.log('adding todo: ', todo);
-        // const todo2 = {text: todo, completed: false }
-        // const { data } = await api.addTodo(todo2);
-
-
-        // console.log('Recieved data: ', data); //should get same results as postman 
-
-        // dispatch({ type: 'ADD_TODO', payload: todo })
-        // return true;
-        // router.push('/');
     } catch (error) {
-        console.log('wrong data');
-        console.log(error);
         return false;
     }
 };
