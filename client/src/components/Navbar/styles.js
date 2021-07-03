5776634653922f428109525086490ac92457d93d
rd-code-23 <70293845+rd-code-23@users.noreply.github.com>
2021-07-03 11:59:41 -0700

    add theme breakpoints

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles((mobile) => ({
+export default makeStyles((theme) => ({
     welcomeMessage: {
         fontSize: '24px',
-        '@media (max-width:720px)': {
+        [theme.breakpoints.down(theme.breakpoints.values.mobile)]: {
             fontSize: '16px',
         }
     },
